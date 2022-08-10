be5a2a456697a229a69e8a3306a749c80368331c
Artyom <csgo1320@gmail.com>
2022-08-10 15:13:28 +0300

    Names

---
+++ Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "артём")
+{
+    Console.WriteLine("Ура, это же Артём");
+}
+else
+{
+    Console.Write ("Привет, ");
+    Console.WriteLine(username);
+}
